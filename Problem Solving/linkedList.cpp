458fb611c2fd20df4caa61d7e0a257e171356123
Raman <k.raman22222@gmail.com>
2021-06-13 18:45:35 +0530

    Linked List Operations added

--- Problem Solving/linkedList.cpp
+++ Problem Solving/linkedList.cpp
@@ -7,6 +7,7 @@ using namespace std;
      int data;
  };
  struct Node * start = NULL;
+
  
  void insertAtLast( int data)
  {
@@ -58,6 +59,47 @@ using namespace std;
      }
      
  }
+ void insertAtPosition(int data , int position)
+ {
+     struct Node * temp = new Node();
+     struct Node * ptr = new Node();
+     ptr= start;
+
+     if(ptr == NULL)
+     {
+         start = temp;
+         temp->data = data;
+         temp->next = NULL;
+     }
+     else
+     {
+         for(int i = 0 ; i<position -1;i++)
+         {
+             ptr = ptr->next;
+         }
+         temp->next =ptr->next;
+         ptr->next = temp;
+         temp->data = data;
+          
+     }
+     
+
+ }
+ void reversePrintList(struct Node *head)
+ {
+     if (head==NULL)
+     {
+         cout<<"The Linked List is empty"<<endl;
+     }
+     else
+     {
+         reversePrintList(head->next);
+         cout<<head->data<<" ";
+     }
+     cout<<endl;
+     
+ }
+
  void PrintList (struct Node * head)
  {
      
@@ -79,14 +121,16 @@ using namespace std;
 
 int main(int argc, const char** argv) {
     
-    insertAtLast(2);
+    insertAtLast(1);
     insertAtLast(3);
     insertAtBeginning(10);
+    insertAtPosition(2,2);
     insertAtLast(3);
     insertAtLast(4);
     insertAtBeginning(40);
     insertAtLast(5);
     PrintList(start);
+    reversePrintList(start);
 
 
     return 0;
